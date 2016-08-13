9d4884e3cd7df4b943f0c0533487d8dd8542d915
chucklqsun <bartowski.sun@gmail.com>
2016-08-13 19:55:19 +0800

    newly create

---
+++ app/src/main/java/info/bartowski/easteregg/MainActivity.java
@@ -0,0 +1,30 @@
+package info.bartowski.easteregg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioGroup;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openEasterEgg(View view){
+        RadioGroup rg = (RadioGroup) findViewById(R.id.egg_choose_rg);
+        Map<Integer,Class> targetMap = new HashMap<>();
+
+        targetMap.put(R.id.egg_dessertcase_rb,DessertCase.class);
+        targetMap.put(R.id.egg_mland_rb,MLandActivity.class);
+
+        Intent intent = new Intent(this, targetMap.get(rg.getCheckedRadioButtonId()));
+        startActivity(intent);
+    }
+}
